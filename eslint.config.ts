17d0436bad0aab9d721c4657798310f3db565c6e
Teages <Teages@teages.xyz>
2026-04-18 22:35:23 +0800

    feat: use `v` as alias for `ü`

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,7 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  ignores: ignores => [...ignores, './skills/**'],
+  ignores: ignores => [...ignores, './skills/**', '.kilo/**'],
   rules: {
     curly: ['error', 'all'],
   },
